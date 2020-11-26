4c0ab0ea9414b8e351f904dcdb31dfc0197c87c6
Britney O'Dell <brodell@microsoft.com>
2020-11-25 16:49:13 -0800

    Adding Settings page

---
+++ WeatherKit/Controllers/SettingsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherKit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherKit.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private SettingOptions so = new SettingOptions();
+
+        Forecast newForecast = new Forecast();
+
+        public IActionResult Index()
+        {
+            return View(so);
+        }
+
+        [HttpPost]
+
+        public IActionResult SaveSettings(string unitOption, string timeFormatOption)
+        {
+            // Get the location input data from cookie
+            // Update setting model
+            // Make API call to get updated forecast 
+            // return View("GetWeatherDetails", newForecast);
+
+            return View("Index", so);// (Placeholder for temp functionality) Delete this line when other updates have been made
+        }
+    }
+}
